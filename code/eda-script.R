2080726e3c0500a9223aea6b3cb7dcd1501d05ef
Bryana Gutierrez <bygutierrez@berkeley.edu>
2016-10-04 19:05:08 -0700

    created eda-script.R file and created all of the output files that are created by the R code i.e. eda-output.txt and the histogram images

---
+++ code/eda-script.R
@@ -0,0 +1,42 @@
+#Loading data
+advertising <- read.csv("http://www-bcf.usc.edu/~gareth/ISL/Advertising.csv")
+
+
+#Creating the summary text file
+sink(file = "data/eda_output.txt")
+cat("These are the summary statistics for TV advertising budgets\n")
+summary(advertising$TV)
+cat("\nThese are the summary statistics for product sales\n")
+summary(advertising$Sales)
+sink()
+
+#Creating histograms and loading them to the images folder. 
+require(ggplot2)
+
+png(filename = "images/histogram-tv.png")
+ggplot(data = advertising) +
+  geom_bar(aes(TV), binwidth = 50, col = "white", fill = "purple") +
+  xlab("TV Advertising Budgets") +
+  ylab("Count")
+dev.off()
+  
+pdf(file = "images/histogram-tv.pdf")
+ggplot(data = advertising) +
+  geom_bar(aes(TV), binwidth = 50, col = "white", fill = "purple") +
+  xlab("TV Advertising Budgets") +
+  ylab("Count")
+dev.off()
+
+png(filename = "images/histogram-sales.png")
+ggplot(data = advertising) +
+  geom_bar(aes(Sales), binwidth = 2, col = "white", fill = "darkgreen") +
+  xlab("Product Sales") +
+  ylab("Count")
+dev.off()
+
+pdf(file = "images/histogram-sales.pdf")
+ggplot(data = advertising) +
+  geom_bar(aes(Sales), binwidth = 2, col = "white", fill = "darkgreen") +
+  xlab("Product Sales") +
+  ylab("Count")
+dev.off()
